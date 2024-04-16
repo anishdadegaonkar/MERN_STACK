fc051596d8d0bccd02ee3120b866473b8f97ff12
anishdadegaonkar <dadegaonkaranish@gmail.com>
2024-04-16 15:16:56 +0530

    ok

--- JavaScript/beginner_projects/Random_Password_Generator/app.js
+++ JavaScript/beginner_projects/Random_Password_Generator/app.js
@@ -2,9 +2,9 @@ var password = document.getElementById("password");
 
 function genPassword(){
     var char = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 8;
-    var password = ""
-    for (var i=0; i<=passwordLength; i++ ){
+    var passwordLength = 12;
+    var password = "";
+    for (var i=1; i<=passwordLength; i++ ){
         var randomPassword = Math.floor(Math.random() * char.length);
         password += char.substring(randomPassword , randomPassword +1)
     }
