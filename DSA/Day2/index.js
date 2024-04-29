68c69d1373c7101624a3e15fe7a98c44562d4e11
anishdadegaonkar <dadegaonkaranish@gmail.com>
2024-04-29 23:01:05 +0530

    js

---
+++ DSA/Day2/index.js
@@ -0,0 +1,41 @@
+// Day 2 : notes
+// basic data types in js are - number , string , boolean
+// var -
+// let -
+// const - connot be changed
+// we can check data type using (typeof)
+// we can conver string to number and viceversa by Number(a) or String(a)
+
+// const pi = 3.14;
+// var name = "testing";
+// name = "wisdom";
+// name = 123;
+
+// console.log(pi);
+// console.log(name);
+// console.log(typeof pi);
+
+// how to declare array
+// let array = [1, 2, 3];
+// console.log(array);
+// console.log(typeof array); // ans = object
+// how to calculate length of array
+
+// how to access array members
+// console.log(array.length); // ans = 3
+// console.log(array[0]); // ans = 1
+
+// const myarray = ["a", "b", "c", "d"];
+// console.log(myarray[1]);
+// console.log(myarray[2]);
+// console.log(myarray[3]);
+// console.log(myarray[4]);
+
+// above example can be done with optimal solution
+// for (let i = 0; i < myarray.length; i++) {
+//   console.log(myarray[i]);
+// }
+
+
+
+
