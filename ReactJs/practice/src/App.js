beddb74e5da424208733fcc01b02c275471e90d5
Anish Dadegaonkar <dadegaonkaranish@gmail.com>
2024-07-08 13:14:40 +0530

    React-Pratice

---
+++ ReactJs/practice/src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+function App() {
+  const txtalign = {
+    textAlign: "center"
+  }
+  return (
+    <div className='container'>
+      <h1 style={txtalign}>Hellow World</h1>
+      <p style={txtalign}>This is a simple react Application</p>
+    </div>
+  );
+}
+
+export default App;
